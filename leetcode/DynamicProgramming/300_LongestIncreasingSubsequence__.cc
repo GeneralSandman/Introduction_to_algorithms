818eda589cf71a7c469eb3d1ac771ce444568fd0
GeneralSandman <13287069774@163.com>
2017-06-19 23:04:47 +0800

    1

--- leetcode/DynamicProgramming/300_LongestIncreasingSubsequence__.cc
+++ leetcode/DynamicProgramming/300_LongestIncreasingSubsequence__.cc
@@ -1,17 +1,43 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<iterator>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
+class Solution
+{
+  public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        if (nums.empty())
+            return 0;
+        int result = 0;
 
+        int begin = 0, end = nums.size() - 1;
 
-int main(){
+        while (begin <= end)
+        {
+            int find = findMiddle(nums, begin, end);
+            if (find > result)
+                result = find;
+
+            if()
+        }
+
+        return result;
+    }
+
+  private:
+    int findMiddle(vector<int> &nums, int begin, int end)
+    {
+    }
+};
+
+int main()
+{
     Solution a;
-    
+
     return 0;
 }
-
-
