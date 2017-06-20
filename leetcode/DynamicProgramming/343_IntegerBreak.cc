49feb74adcb46086c84083d9bb52fbe96ca8fff5
GeneralSandman <13287069774@163.com>
2017-06-20 23:37:09 +0800

    add 343

--- leetcode/DynamicProgramming/343_IntegerBreak.cc
+++ leetcode/DynamicProgramming/343_IntegerBreak.cc
@@ -15,8 +15,11 @@ class Solution
         nums[1] = 1;
         for (int i = 2; i < n + 1; i++)
         {
-            for (int j=1;j<=i-1;j++)
-                nums[i] = max(nums[i], max(nums[i-j] * nums[j], (i-j) * j));
+            for (int j=1;j<=i-1;j++){
+                int max1=max(nums[i-j]*nums[j],(i-j)*j);
+                int max2=max(nums[i-j]*j,(i-j)*nums[j]);
+                nums[i] = max(nums[i],max(max1,max2));
+            }
         }
 
         return nums[n];
