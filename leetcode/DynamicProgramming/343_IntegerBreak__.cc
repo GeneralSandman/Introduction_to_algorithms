6847bd145d13621e5ae0ef08f0568d66be2155a5
GeneralSandman <13287069774@163.com>
2017-06-20 23:28:25 +0800

    343

--- leetcode/DynamicProgramming/343_IntegerBreak__.cc
+++ leetcode/DynamicProgramming/343_IntegerBreak__.cc
@@ -1,17 +1,34 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<iterator>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
+class Solution
+{
+  public:
+    int integerBreak(int n)
+    {
+        vector<int> nums(n + 1, 0);
+        nums[1] = 1;
+        for (int i = 2; i < n + 1; i++)
+        {
+            for (int j=1;j<=i-1;j++)
+                nums[i] = max(nums[i], max(nums[i-j] * nums[j], (i-j) * j));
+        }
 
+        return nums[n];
+    }
+};
 
-int main(){
+int main()
+{
     Solution a;
-    
+    int n;
+    cin>>n;
+    cout<<a.integerBreak(n)<<endl;
+
     return 0;
 }
-
-
