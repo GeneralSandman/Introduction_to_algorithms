b3da0bbc163f37f41b0bc3c8be297ff8402dd1e9
GeneralSandman <13287069774@163.com>
2017-07-17 00:23:39 +0800

    120

---
+++ leetcode/DynamicProgramming/120_Triangle.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+class Solution
+{
+  public:
+    int minimumTotal(vector<vector<int>> &triangle)
+    {
+        if(triangle.empty())
+            return 0;
+
+
+        for(int i=triangle.size()-2;i>=0;i--){
+            for(int j=0;j<triangle[i].size();j++){
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
+            }
+        }
+
+        return triangle[0][0];
+    }
+
+};
+
+int main()
+{
+    Solution a;
+
+    return 0;
+}
