740149c6746f761e28fa5eceb73bf2525e288d30
GeneralSandman <13287069774@163.com>
2017-07-28 17:44:56 +0800

    127

---
+++ leetcode/127_WordLadder__.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+
+class Solution
+{
+  public:
+    int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+    {
+        vector<string> cur_level, next_level;
+        cur_level.push_back(beginWord);
+        int depth = 1;
+        int len = beginWord.size();
+        while (!cur_level.empty())
+        {
+            for (auto cur_word : cur_level)
+            {
+                if (cur_word == endWord)
+                    return depth;
+                for (int i = 0; i < len; i++)
+                {
+                    for (char c = 'a'; c <= 'z'; c++)
+                    {
+                        cur_word[i] = c;
+
+                        auto p = find(wordList.begin(), wordList.end(), cur_word);
+                        if (p != wordList.end())
+                        {
+                            next_level.push_back(cur_word);
+                            wordList.erase(p);
+                        }
+                    }
+                }
+            }
+
+            depth++;
+            cur_level = next_level;
+            next_level.erase(next_level.begin(), next_level.end());
+        }
+
+        return 0;
+    }
+};
+
+int main()
+{
+    Solution a;
+
+    return 0;
+}
