4f645bf15ea9a3c3272e13d9a921ba09bf98fd2c
GeneralSandman <13287069774@163.com>
2017-08-01 22:57:02 +0800

    207

--- leetcode/Breadth-firstSearch/207_CourseSchedule.cc
+++ leetcode/Breadth-firstSearch/207_CourseSchedule.cc
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+//BFS
 class Solution
 {
   public:
@@ -47,6 +48,42 @@ class Solution
         for (auto t : prerequisites)
 
             inGreed[t.first]++;
+    }
+};
+
+//DFS
+class Solution2
+{
+  public:
+    bool canFinish(int numCourses, vector<pair<int, int>> &prerequisites)
+    {
+        if (!numCourses)
+            return false;
+
+        vector<set<int>> graph(numCourses); //first:course second:needed
+        vector<bool> visited(numCourses, false), onpath(numCourses, false);
+        makeGraph(prerequisites, graph);
+
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i] && dfs(graph,i,visited,onpath))
+                
+        }
+
+        return true;
+    }
+
+  private:
+    void makeGraph(vector<pair<int, int>> &prerequisites, vector<set<int>> &res)
+    {
+        for (auto t : prerequisites)
+            res[t.second].insert(t.first);
+    }
+
+    bool dfs(vector<set<int>>&graph,int node,vector<bool>&visited,vector<bool>&onpath){
+        onpath[node]=visited[node]=true;
+        for(auto t:graph[node]){
+            if(dfs(graph,t,visited,onpath))
+        }
     }
 };
 
