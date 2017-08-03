16fbaeadcf34d829433873f69d49731fffaf5dfb
GeneralSandman <13287069774@163.com>
2017-08-03 15:25:26 +0800

    add 279

---
+++ leetcode/DynamicProgramming/279_PerfectSquares.cc
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<iterator>#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <climits>
+#include <cmath>
+using namespace std;
+
+//Dynamic Programming
+class Solution
+{
+  public:
+    int numSquares(int n)
+    {
+        if (n <= 0)
+            return 0;
+
+        vector<int> res(n + 1, INT_MAX);
+        for (int i = 1; i < n + 1; i++)
+        {
+            int q=sqrt(i);
+            if (q*q == i)
+            {
+                res[i] = 1;
+                continue;
+            }
+            for (int j = 1; j*j < i; j++)
+                res[i] = min(res[i], 1 + res[i - j*j]);       
+            
+
+            /*
+            //Time Limit Exceeded
+            for(int j=1;j<i;j++)
+                res[i]=min(res[i],res[i]+res[i-j]);
+            */
+        }
+
+        return res[n];
+    }
+};
+
+int main()
+{
+    Solution a;
+    for (int i = 0; i <= 8829; i++)
+        cout << i << ":" << a.numSquares(i) << endl;
+
+    return 0;
+}
+
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int numSquares(int n) {
+        
+    }
+};
+
+int main(){
+    Solution a;
+    
+    return 0;
+}
+
+
