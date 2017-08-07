d5e597dfbdd0e2466d9b9e0dfd6946a849a66b15
GeneralSandman <13287069774@163.com>
2017-08-07 20:33:01 +0800

    a

--- leetcode/Depth-firstSearch/114_FlattenBinaryTreetoLinkedList__.cc
+++ leetcode/Depth-firstSearch/114_FlattenBinaryTreetoLinkedList__.cc
@@ -6,6 +6,21 @@
 
 using namespace std;
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        
+    }
+};
 
 
 int main(){
--- leetcode/Tree/563_BinaryTreeTilt__.cc
+++ leetcode/Tree/563_BinaryTreeTilt__.cc
@@ -6,7 +6,20 @@
 
 using namespace std;
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        
+    }
+};
 
 int main(){
     Solution a;
