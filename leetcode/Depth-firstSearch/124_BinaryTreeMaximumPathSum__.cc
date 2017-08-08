7a737d8eafe4090c70866b4cc24add1a307ad428
GeneralSandman <13287069774@163.com>
2017-08-08 12:10:01 +0800

    ass

--- leetcode/Depth-firstSearch/124_BinaryTreeMaximumPathSum__.cc
+++ leetcode/Depth-firstSearch/124_BinaryTreeMaximumPathSum__.cc
@@ -1,17 +1,43 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<iterator>
-#include<algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
 
 using namespace std;
 
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
 
-int main(){
+class Solution
+{
+private:
+    int maxValue;
+  public:
+    int maxPathSum(TreeNode *root)
+    {
+    }
+    private:
+    int maxPathDown(TreeNode *node){
+        if(node==nullptr)
+            return 0;
+        
+        int left=max(maxPathDown(node->left),0);
+        int right=max(maxPathDown(node->right),0);
+        
+        return max(left,right)+node->val;
+    }
+};
+
+int main()
+{
     Solution a;
-    
+
     return 0;
 }
-
-
