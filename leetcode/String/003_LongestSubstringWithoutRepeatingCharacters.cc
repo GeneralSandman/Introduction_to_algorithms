046a27cd9f890b1274e4ccf53dbc86853523f62a
GeneralSandman <13287069774@163.com>
2017-08-17 22:24:21 +0800

    add 003

--- leetcode/String/003_LongestSubstringWithoutRepeatingCharacters.cc
+++ leetcode/String/003_LongestSubstringWithoutRepeatingCharacters.cc
@@ -1,33 +1,40 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<iterator>
-#include<algorithm>
-#include<set>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <set>
+#include <map>
 using namespace std;
 
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        vector<int>dict(256,-1);
-        int len=0;
-        int start=-1;
-        for(int i=0;i<s.size();i++){
-            if(dict[s[i]]>start){
-                start=dict[s[i]];
+class Solution
+{
+  public:
+    int lengthOfLongestSubstring(string s)
+    {
+        vector<int> dict(256, -1);
+        int len = 0;
+        int start = -1;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (dict[s[i]] > start)
+            {
+                start = dict[s[i]];
             }
 
-            dict[s[i]]=i;
-            len=max(len,i-start);
+            dict[s[i]] = i;
+            len = max(len, i - start);
         }
         return len;
     }
 };
 
-int main(){
+
+
+int main()
+{
     Solution a;
-    cout<<a.lengthOfLongestSubstring("abcabcbb")<<endl;
+    // cout << a.lengthOfLongestSubstring("abcdefg") << endl;
+    cout << a.lengthOfLongestSubstring("abba") << endl;
     return 0;
 }
-
-
